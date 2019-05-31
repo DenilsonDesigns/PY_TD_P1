c4c8ae21546e38521f6f30923f33c35afbd81c51
Languiller <Daniel.Languiller@team.telstra.com>
2019-05-31 11:15:04 +1000

    finished exceeds i think

--- guessing_game.py
+++ guessing_game.py
@@ -2,36 +2,68 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces.
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
 
+# variable for high score
+global_high_score = None
 
-def start_game():
-    """Psuedo-code Hints
 
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
+def start_game(current_high_score):
+    # Psuedo-code Hints
+    # When the program starts, we want to:
+    # ------------------------------------
+    # 1. Display an intro/welcome message to the player.
+    print("Welcome to PyGuess")
+    print("==================")
+    # 2. Store a random number as the answer/solution.
+    answer = random.randint(1, 10)
+    # generate variable to hold num of guesses
+    num_guesses = 0
+    # boolean to break loop
+    guessed_correct = False
+    # high score var
+    high_score = current_high_score
+    global global_high_score
+    # Continuously prompt player to guess until correct
+    while not guessed_correct:
+        try:
+            print("Guess a number between 1 and 10")
+            guess = input()
+            guess = int(guess)
+        except ValueError:
+            print("Please enter only integers between 1 and 10 (inclusive)")
+            continue
+        if guess > 10 or guess < 1:
+            print("Please choose a number between 1 and 10 (inclusive)")
+            continue
+        num_guesses += 1
+        if guess < answer:
+            print("It's higher")
+        elif guess > answer:
+            print("It's lower")
+        if guess == answer:
+            print("You guessed correct! It took you {} tries!".format(num_guesses))
+            print("Exiting game: Thanks for playing! :)")
+            guessed_correct = True
+            if high_score:
+                if num_guesses < high_score:
+                    high_score = num_guesses
+                    global_high_score = high_score
+            if not high_score:
+                high_score = num_guesses
+                global_high_score = high_score
+            print("Current high score:", high_score)
 
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+    if guessed_correct:
+        print("Would you like to play again? (Y/N)")
+        decision = input()
+        decision = str(decision)
+        if (decision == 'Y') or (decision == 'y'):
+            start_game(global_high_score)
 
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
-    start_game()
+    start_game(global_high_score)
